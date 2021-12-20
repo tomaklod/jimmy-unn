f5334e01f69315947c7b1fa3b6138e799a7f1ed8
Thomas <thomas.cla.sto@gmail.com>
2021-12-20 15:17:40 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
       perPage: 3,
       focus: 'center',
       autoplay: true,
-      interval: 8000,
+      interval: 4000,
       flickMaxPages: 3,
       updateOnMove: true,
       pagination: false,
@@ -12,6 +12,10 @@ document.addEventListener( 'DOMContentLoaded', function () {
       throttle: 300,
       breakpoints: {
         1440: {
+          perPage: 1,
+          padding: '3%'
+        },
+        1900: {
           perPage: 1,
           padding: '30%'
         }
